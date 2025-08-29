9d3840f1f3ef26c54924460035616457df24dc55
Krrishna <codewithalpha@outlook.com>
2025-08-29 13:05:17 +0500

    Add decimal to binary converter program

---
+++ decimal_to_binary_converter.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+void decToBinary(int n) {
+    // array to store binary number
+    int binaryNum[32];
+
+    // counter for binary array
+    int i = 0;
+    while (n > 0) {
+        // storing remainder in binary array
+        binaryNum[i] = n % 2;
+        n = n / 2;
+        i++;
+    }
+
+    // printing binary array in reverse order
+    for (int j = i - 1; j >= 0; j--)
+        printf("%d", binaryNum[j]);
+}
+
+int main() {
+    int n;
+    printf("Enter a decimal number: ");
+    scanf("%d", &n);
+    printf("Binary equivalent: ");
+    decToBinary(n);
+    printf("\n");
+    return 0;
+}
+
